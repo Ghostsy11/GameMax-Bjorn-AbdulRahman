e4ac4f2fda27642fda400fa046a14093573d1804
BjornWarfman <warfmanb@hotmail.nl>
2024-09-25 13:30:31 +0200

    Camera and Player Movement

---
+++ Assets/Scripts/SC_PlayerMovement.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_PlayerMovement : MonoBehaviour
+{
+
+    private float moveSpeed;
+    [Header("Movement")]
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private float sprintingSpeed;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float airMultiplier;
+
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    private bool readyToJump;
+
+
+    [Header("KeyBindings")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool grounded;
+
+
+    [SerializeField] private Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private MovementState state;
+
+    private enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
+    }
+
+
+    void Update()
+    {
+        grounded = Physics.CheckSphere(GroundCheck.position, groundDistance, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+        StateHandler();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void StateHandler()
+    {
+        // mode - sprinting
+        if(Input.GetKey(sprintKey) && grounded)
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintingSpeed;
+        }
+        
+        // mode - walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = movingSpeed;
+        }
+
+        //mode - air
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        //in air
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+        Vector3 limitedVel = flatVel.normalized * moveSpeed;
+        rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
