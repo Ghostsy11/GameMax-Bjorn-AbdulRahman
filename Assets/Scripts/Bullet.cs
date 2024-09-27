2ce20782ea2395184bc8e6479e2181a367174403
Ghostsy11 <aboodaysy123@gmail.com>
2024-09-27 10:02:59 +0200

    1

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,14 +8,15 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     protected Rigidbody rb;
     protected GameObject player;
-    [SerializeField]private float bulletSpeed = 10;
+    [SerializeField] private float bulletSpeed = 10;
     void Start()
     {
         player = SC_PlayerMovement.Instance.gameObject;
         rb = GetComponent<Rigidbody>();
         rb.AddRelativeForce(Vector3.forward * bulletSpeed);
     }
-    protected virtual void OnTriggerEnter(Collider collider){
+    protected virtual void OnTriggerEnter(Collider collider)
+    {
         if (!collider.CompareTag("Player"))
         {
             Debug.Log("dead");
